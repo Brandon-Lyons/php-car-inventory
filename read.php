2c1060bb9b282875be480588f82c86cc8fa4cd7a
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-08-19 17:24:42 -0500

    added edit and delete buttons to read view

--- read.php
+++ read.php
@@ -25,7 +25,11 @@
     </div>
     <div class="row">
       <p><?php echo $data['info'] ?></p>
-      <a href="index.php" class="btn">Back</a>
+      <div class="btn-group">
+        <a class="btn" href="update.php?id=<?php echo $data['id'] ?>">Edit</a>
+        <a class="btn btn-danger" href="delete.php?id=<?php echo $data['id'] ?>">Delete</a>
+        <a href="index.php" class="btn">Back</a>
+      </div>
     </div>
   </div>
 </div> <!-- /container -->
